f2b3db4853c29201a49e55c045064860a79a686a
Dhruuuuuuv <dhruvkumar787898@gmail.com>
2026-05-04 18:03:06 +0530

    Time: 12 ms (48.10%) | Memory: 70.8 MB (65.80%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize) {
+    int* result = (int*)calloc(temperaturesSize, sizeof(int));
+    int* stack = (int*)malloc(temperaturesSize * sizeof(int));
+    int top = -1;
+    *returnSize = temperaturesSize;
+
+    for (int i = 0; i < temperaturesSize; i++) {
+        while (top > -1 && temperatures[i] > temperatures[stack[top]]) {
+            int prevIndex = stack[top--];
+            result[prevIndex] = i - prevIndex;
+        }
+        stack[++top] = i;
+    }
+
+    free(stack);
+    return result;
+}
